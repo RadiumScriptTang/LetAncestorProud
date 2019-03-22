e9d9afc3287fd01dd9b2978f6484b8a9044cf81f
RadiumScriptTang <RadiumScriptTang@163.com>
2019-03-23 00:57:25 +0800

    优化了用户表达式和删除方面的细节

--- app/src/main/java/com/example/radiumscripttang/software_engineer/MainActivity.java
+++ app/src/main/java/com/example/radiumscripttang/software_engineer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.radiumscripttang.software_engineer;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 
 
@@ -41,30 +43,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn_sin, btn_cos, btn_tan, btn_lg, btn_ln, btn_pow, btn_square, btn_cube, btn_pi, btn_euler, btn_sqrt;
     private TextView e_input;
     private StringBuilder formula = new StringBuilder();
+    private StringBuilder expression = new StringBuilder();
 
     private void initView() {
-        btn_0 = (Button) findViewById(R.id.btn_0);
-        btn_1 = (Button) findViewById(R.id.btn_1);
-        btn_2 = (Button) findViewById(R.id.btn_2);
-        btn_3 = (Button) findViewById(R.id.btn_3);
-        btn_4 = (Button) findViewById(R.id.btn_4);
-        btn_5 = (Button) findViewById(R.id.btn_5);
-        btn_6 = (Button) findViewById(R.id.btn_6);
-        btn_7 = (Button) findViewById(R.id.btn_7);
-        btn_8 = (Button) findViewById(R.id.btn_8);
-        btn_9 = (Button) findViewById(R.id.btn_9);
-        btn_point = (Button) findViewById(R.id.btn_point);
-        btn_clear = (Button) findViewById(R.id.btn_clear);
-        btn_del = (Button) findViewById(R.id.btn_del);
-        btn_plus = (Button) findViewById(R.id.btn_plus);
-        btn_minus = (Button) findViewById(R.id.btn_minus);
-        btn_multiply = (Button) findViewById(R.id.btn_mul);
-        btn_divide = (Button) findViewById(R.id.btn_divide);
-        btn_equal = (Button) findViewById(R.id.btn_equal);
-        btn_mod = (Button) findViewById(R.id.btn_mod);
+        btn_0 =  findViewById(R.id.btn_0);
+        btn_1 =  findViewById(R.id.btn_1);
+        btn_2 =  findViewById(R.id.btn_2);
+        btn_3 =  findViewById(R.id.btn_3);
+        btn_4 =  findViewById(R.id.btn_4);
+        btn_5 =  findViewById(R.id.btn_5);
+        btn_6 =  findViewById(R.id.btn_6);
+        btn_7 =  findViewById(R.id.btn_7);
+        btn_8 =  findViewById(R.id.btn_8);
+        btn_9 =  findViewById(R.id.btn_9);
+        btn_point =  findViewById(R.id.btn_point);
+        btn_clear =  findViewById(R.id.btn_clear);
+        btn_del =  findViewById(R.id.btn_del);
+        btn_plus =  findViewById(R.id.btn_plus);
+        btn_minus =  findViewById(R.id.btn_minus);
+        btn_multiply =  findViewById(R.id.btn_mul);
+        btn_divide =  findViewById(R.id.btn_divide);
+        btn_equal =  findViewById(R.id.btn_equal);
+        btn_mod =  findViewById(R.id.btn_mod);
         e_input = (TextView) findViewById(R.id.input);
-        btn_left = (Button) findViewById(R.id.btn_left);
-        btn_right = (Button) findViewById(R.id.btn_right);
+        btn_left =  findViewById(R.id.btn_left);
+        btn_right =  findViewById(R.id.btn_right);
         btn_sin = findViewById(R.id.btn_sin);
         btn_cos = findViewById(R.id.btn_cos);
         btn_tan = findViewById(R.id.btn_tan);
@@ -129,106 +132,158 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.btn_0:
                 formula.append("0");
+                expression.append("0");
                 break;
             case R.id.btn_1:
                 formula.append("1");
+                expression.append("1");
                 break;
             case R.id.btn_2:
                 formula.append("2");
+                expression.append("2");
                 break;
             case R.id.btn_3:
                 formula.append("3");
+                expression.append("3");
                 break;
             case R.id.btn_4:
                 formula.append("4");
+                expression.append("4");
                 break;
             case R.id.btn_5:
                 formula.append("5");
+                expression.append("5");
                 break;
             case R.id.btn_6:
                 formula.append("6");
+                expression.append("6");
                 break;
             case R.id.btn_7:
                 formula.append("7");
+                expression.append("7");
                 break;
             case R.id.btn_8:
                 formula.append("8");
+                expression.append("8");
                 break;
             case R.id.btn_9:
                 formula.append("9");
+                expression.append("9");
                 break;
             case R.id.btn_plus:
                 //if (last >= '0' && last <= '9' ) {
                 formula.append("+");
+                expression.append("+");
                 // }
                 break;
             case R.id.btn_minus:
                 //if (last >= '0' && last <= '9') {
                 formula.append("-");
+                expression.append("-");
                 //  }
                 break;
             case R.id.btn_mul:
                 // if (last >= '0' && last <= '9' ) {
                 formula.append("*");
+                expression.append("×");
                 // }
                 break;
             case R.id.btn_divide:
                 // if (last >= '0' && last <= '9' ) {
                 formula.append("/");
+                expression.append("/");
                 // }
                 break;
             case R.id.btn_sin:
                 formula.append("sin(");
+                expression.append("sin(");
                 break;
             case R.id.btn_cos:
                 formula.append("cos(");
+                expression.append("cos(");
                 break;
             case R.id.btn_tan:
                 formula.append("tan(");
+                expression.append("tan(");
                 break;
             case R.id.btn_point:
                 if (judge_point()) {
                     formula.append(".");
+                    expression.append(".");
                 }
                 break;
             case R.id.btn_lg:
                 formula.append("lg(");
+                expression.append("lg(");
                 break;
             case R.id.btn_ln:
                 formula.append("ln(");
+                expression.append("ln(");
                 break;
             case R.id.btn_euler:
                 formula.append("euler");
+                expression.append("e");
                 break;
             case R.id.btn_pi:
                 formula.append("pi");
+                expression.append("π");
                 break;
             case R.id.btn_pow:
                 formula.append("^");
+                expression.append("^");
                 break;
             case R.id.btn_square:
                 formula.append("^2");
+                expression.append("^2");
                 break;
             case R.id.btn_cube:
                 formula.append("^3");
+                expression.append("^3");
                 break;
             case R.id.btn_sqrt:
                 formula.append("sqrt(");
+                expression.append("√(");
                 break;
             case R.id.btn_left:
                 formula.append("(");
+                expression.append("(");
                 break;
             case R.id.btn_right:
                 formula.append(")");
+                expression.append(")");
                 break;
             case R.id.btn_del: //删除
                 if (formula.length() != 0) {
-                    formula.delete(formula.length() - 1, formula.length());
+//                    formula.delete(formula.length() - 1, formula.length());
+                    if (formula.length() > 1 && formula.substring(formula.length() - 2).equals("pi")) {
+                        //最后一个是pi
+                        formula.delete(formula.length() - 2, formula.length());
+                        expression.delete(expression.length() - 1, expression.length());
+                    } else if (formula.length() > 4 && (formula.substring(formula.length() - 5).equals("euler"))) {
+                        //最后一个是自然对数的底
+                        formula.delete(formula.length() - 5, formula.length());
+                        expression.delete(expression.length() - 1, expression.length());
+                    } else if (formula.length() > 3 && (formula.substring(formula.length() - 4).equals("sin(") || formula.substring(formula.length() - 4).equals("cos(") || formula.substring(formula.length() - 4).equals("tan("))) {
+                        //最后一位是sin( cos( tan(
+                        formula.delete(formula.length() - 4, formula.length());
+                        expression.delete(expression.length() - 4, expression.length());
+                    } else if (formula.length() > 2 && (formula.substring(formula.length() - 3).equals("lg(") || formula.substring(formula.length() - 3).equals("ln("))) {
+                        //最后一个是求对数
+                        formula.delete(formula.length() - 3, formula.length());
+                        expression.delete(expression.length() - 3, expression.length());
+                    } else if (formula.length() > 4 && formula.substring(formula.length() - 5).equals("sqrt(")){
+                        //最后一个是开根号
+                        formula.delete(formula.length() - 5, formula.length());
+                        expression.delete(expression.length() - 2, expression.length());
+                    } else {
+                        formula.delete(formula.length() - 1,formula.length());
+                        expression.delete(expression.length() - 1,expression.length());
+                    }
                 }
                 break;
             case R.id.btn_clear: //清空
                 formula.delete(0, formula.length());
-                
+                expression.delete(0,expression.length());
                 break;
             case R.id.btn_equal: // =等于
                 if ((formula.length() > 1)) {
@@ -238,10 +293,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         String a = formula.toString();
                         BigDecimal result_num = Parser.parse(a).evaluate();
                         result = String.valueOf(Double.parseDouble(result_num.toString()));
+                        expression.delete(0,expression.length());
+                        expression.append(result);
                     } catch (Exception ex) {
                         result = "出错";
+                        expression.delete(0,expression.length());
+                        expression.append(result);
                     }
-                    e_input.setText(formula + "=" + result);
+//                    expression.append()
                     formula.delete(0, formula.length());
                     if (Character.isDigit(result.charAt(0))) {
                         formula.append(result);
@@ -251,7 +310,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
         }
-        e_input.setText(formula);
+        e_input.setText(expression);
+        Log.i("MAIN", "onClick: formula " + formula.length());
+        Log.i("MAIN", "onClick: expression " + expression.length() );
     }
     //计算式最后一个符号不能是.
     private boolean judge_point() {
